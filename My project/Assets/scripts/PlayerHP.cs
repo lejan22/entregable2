f27cc35bdfc7a3b64bc0c69c00ee93750a29f029
lejan22 <91455326+lejan22@users.noreply.github.com>
2021-10-25 17:01:55 +0200

    entregable2

---
+++ My project/Assets/scripts/PlayerHP.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHP : MonoBehaviour
+{
+    [SerializeField] private bool isGameOver;
+[SerializeField] private int playerHP = 150;
+[SerializeField] public int damage = 60;
+
+
+ 
+    
+// Start is called before the first frame update
+    void Start()
+    {
+       Debug.Log(playerHP - damage);
+ 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerHP <= 0)
+        {
+            Debug.Log("Te has quedado sin vida...GAMEOVER");
+        }else if (playerHP < 30)
+        {
+            Debug.Log("¡¡¡Tienes poca vida!!!");
+        }
+        else
+        {
+            Debug.Log("Vas bien de vida: tienes {playerHP} puntos de vida");
+        }
+        if (isGameOver = false)
+        {
+            Debug.Log("GAME OVER");
+        }
+
+    }
+
+
+}
